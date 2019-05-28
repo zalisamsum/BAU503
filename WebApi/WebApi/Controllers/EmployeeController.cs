6c1edad1ff7a795d7c917090b591574004b7d095
Zülfikar Ali SAMSUM <zalisamsum@gmail.com>
2019-05-28 16:56:02 +0300

    WebApi
    WebApi

---
+++ WebApi/WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        static List<Employee> Employees = new List<Employee>()
+        {
+            new Employee(){id=1,Name="Person1"},
+            new Employee(){id=2,Name="Person2"},
+            new Employee(){id=3,Name="Person3"},
+
+        };
+        public IEnumerable<Employee> Get()
+        {
+            return Employees;
+
+        }
+        public Employee Get(int id)
+        {
+            return Employees.FirstOrDefault(x => x.id == id);
+        }
+        [Route("api/employee/{id}/tasks")]
+        public IEnumerable<string> GetEmployeeTask(int id)
+        {
+            switch (id)
+            {
+                case 1: return new List<string> { "Task1-1", "Task1-2", "Task1-3" };
+                    
+                case 2: return new List<string> { "Task1-1", "Task1-2", "Task1-3" };
+
+                case 3: return new List<string> { "Task1-1", "Task1-2", "Task1-3" };
+
+                default: return null;
+
+
+            }
+        }
+    }
+}
---
+++ WebApi/WebApi/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
