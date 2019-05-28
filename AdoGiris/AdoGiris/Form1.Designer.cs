89f078b84463945651b51ea15d3ff3652dfd978d
Zülfikar Ali SAMSUM <zalisamsum@gmail.com>
2019-05-28 16:51:26 +0300

    ado
    addo

---
+++ AdoGiris/AdoGiris/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace AdoGiris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSqlBaglan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSqlBaglan
+            // 
+            this.btnSqlBaglan.Location = new System.Drawing.Point(301, 151);
+            this.btnSqlBaglan.Name = "btnSqlBaglan";
+            this.btnSqlBaglan.Size = new System.Drawing.Size(160, 83);
+            this.btnSqlBaglan.TabIndex = 0;
+            this.btnSqlBaglan.Text = "SQL\'e Bağlan";
+            this.btnSqlBaglan.UseVisualStyleBackColor = true;
+            this.btnSqlBaglan.Click += new System.EventHandler(this.btnSqlBaglan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSqlBaglan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSqlBaglan;
+    }
+}
+
---
+++ AdoGiris/AdoGiris/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoGiris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSqlBaglan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Ado; Activex Data Object -> veri tabanı işlemleri için kullanacağımız yapıdır.
+
+            //connected mimari
+
+            //Disconnected mimari
+
+            /* Veri Çekme Adımları
+             * 1.Bağlantı OLuştur
+             * 2.Sorguyu OLuştur
+             * 3.Bağlantıyı aç.
+             * 4.Sorguyu çalıştır ve veriyi elde et/gönder
+             * 5.Bağlantıyı kapat.
+             */
+        }
+    }
+}
